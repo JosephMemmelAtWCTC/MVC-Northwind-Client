e10f992dbc6be624bb225652b288e97fc67ff0b5
42null <averyparanoiduser@gmail.com>
2024-03-14 16:45:33 -0500

    w/table

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,18 +7,8 @@ public class ProductController(DataContext db) : Controller
     // this controller depends on the DataContext
     private readonly DataContext _dataContext = db;
 
-
-    // public IActionResult Category() => View(_dataContext.Categories.Include(c => c.Products).OrderBy(c => c.CategoryName));
     public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
 
-    // public IActionResult Products(int id) => View(_dataContext.Products.OrderBy(p => p.ProductName));
-
-    public IActionResult Index(int id) => View(_dataContext.Products.OrderBy(b => b.CategoryId == id));
-
-    // public IActionResult Index(int id) => View(new PostViewModel
-    // {
-    //     category = _dataContext.Categories.FirstOrDefault(b => b.CategoryId == id),
-    //     products = _dataContext.Products.Where(p => p.CategoryId == id)
-    // });
+    public IActionResult Index(int id) => View(_dataContext.Products.OrderBy(p => !p.Discontinued && p.CategoryId == id));
 
 }
