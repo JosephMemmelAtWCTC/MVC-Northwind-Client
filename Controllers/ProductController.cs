72609c586f70fbda9576029387e6bed84d60dea9
42null <averyparanoiduser@gmail.com>
2024-03-14 20:37:25 -0500

    Submission Level: Assign 06: Discounts

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -10,5 +10,6 @@ public class ProductController(DataContext db) : Controller
     public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
 
     public IActionResult Index(int id) => View(_dataContext.Products.Where(p => !p.Discontinued && p.CategoryId == id).OrderBy(p => p.ProductName));
+    public IActionResult Discounts() => View(_dataContext.Discounts.Where(d => d.EndTime > DateTime.Today).OrderBy(d => d.EndTime));
 
 }
